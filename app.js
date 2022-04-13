9ecb9f3a4cedd33905d9553d6b8a00d7ca2bf6b0
mechanicworld <47930174+mechanicworld@users.noreply.github.com>
2022-04-13 22:39:03 +0300

    Ready for deployment

--- app.js
+++ app.js
@@ -1,38 +1,59 @@
-const express = require('express')
-const path = require('path')
+const express = require('express');
+const mongoose = require('mongoose');
+const fileUpload = require('express-fileupload');
+const methodOverride = require('method-override');
+
+const ejs = require('ejs');
+const photoController = require('./controllers/photoController')
+const pageController = require('./controllers/pageController')
 
 const app = express();
 
-// const myLogger1 = (req,res,next) => {
-//   console.log("MIddleware Log 1")
-//   next();
-// }
-
-// const myLogger2 = (req,res,next) => {
-//   console.log("MIddleware Log 2")
-//   next();
-// }
-//MIDDLEWARES
-app.use(express.static('public'))
-// app.use(myLogger1);
-// app.use(myLogger2);
-const port = 3000;
-
-
-app.get('/', ((req, res) => {
-  res.sendFile(path.resolve(__dirname, 'temp/index.html'))
-}))
-
-app.get('/photo', ((req, res) => {
-
-  const photo = {
-    id:1,
-    name:"Photo Name"
-  }
-  res.send(photo)
-}))
-
-app.listen(port, () => {
-  console.log(`Server is running at port: ${port}`)
+// CONNECT DB
+mongoose.connect('mongodb+srv://mechanicworld:FmIYBh4W3yoqluiJ@cluster0.jurl3.mongodb.net/pcat-tutorial?retryWrites=true&w=majority')
+  .then(() => {
+    console.log("DB-Connected")
 })
+  .catch((err) => {
+    console.log(err)
+  }) ;
 
+// {
+//   useNewUrlParser:true,
+//   useUnifiedTopology:true,
+// }
+
+// TEMPLATE ENGINE
+app.set('view engine', 'ejs');
+
+// MIDDLEWARES
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(fileUpload());
+app.use(methodOverride('_method', {
+  methods: ['POST', 'GET']
+}));
+// ROUTES
+
+
+app.get('/', photoController.getAllPhotos);
+app.get('/photos/:id', photoController.getPhoto);
+app.post('/photos', photoController.createPhoto);
+app.put('/photos/edit/:id', photoController.updatePhoto);
+app.delete('/photos/:id', photoController.deletePhoto)
+
+
+app.get('/about', pageController.getAboutPage);
+
+app.get('/add', pageController.getAddPage);
+
+app.get('/photos/edit/:id', pageController.getEditPage);
+
+
+
+
+const port = process.env.PORT||5000;
+app.listen(port, () => {
+  console.log(`Server is running at port: ${port}`);
+});
